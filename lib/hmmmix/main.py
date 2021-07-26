71bfdafad8aa842ef04be2734d6c7bea0ae243f0
rfc <reuben.fletchercostin@gmail.com>
2021-07-27 08:36:58 +1000

    really demo on full data set

--- lib/hmmmix/main.py
+++ lib/hmmmix/main.py
@@ -19,7 +19,6 @@ def do_solve(args):
 
     # mip library goes completely bananas if given unsigned integers
     e_hat = numpy.asarray(e_hat, dtype=numpy.int64)
-    e_hat = e_hat[-300:, -3:]
 
     n_time, n_type = e_hat.shape
 
