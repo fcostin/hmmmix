9b64eff7bd839d5481803b49c04a82d50aac6185
rfc <reuben.fletchercostin@gmail.com>
2021-07-27 06:09:20 +1000

    libtrellis - push entire viterbi inside kernel
    This doesn't impact performance (yet).

--- lib/hmmmix/trellis/libtrellis.pyx
+++ lib/hmmmix/trellis/libtrellis.pyx
@@ -1,3 +1,5 @@
+import numpy
+
 ctypedef long index_t
 ctypedef double dtype_t
 
@@ -5,27 +7,45 @@ def _kernel(index_t[:] times, \
             index_t[:] states, \
             outgoing_edges, \
             dtype_t[:] prizes, \
-            dtype_t[:, :] v, \
-            dtype_t[:, :] logprob, \
-            index_t[:, :] parent_s, \
-            index_t[:, :] parent_obs):
+            dtype_t[:] logprob_prior):
     """
     :param times: array of integer time indices [0, ..., T-1] shape (T,)
     :param states: array of state indices [s_1, ..., s_K] shape (S,)
     :param outgoing_edges: dict of state index to list of edge tuples. rework.
     :param prizes: array of float prizes. shape (T, )
-    :param v: array of float value. shape (T, S)
-    :param logprob: array of float logprob. shape (T, S)
-    :param parent_s: array of state indices. shape (T, S)
-    :param parent_obs: array of integer event counts. shape (T, S)
+    :param logprob_prior: array of float value. shape (S. )
 
-    returns None
-
-    v, logprob, parent_s and parent_obs are mutated in place.
+    returns (objective_star, logprob_star, state_trajectory, obs_trajectory)
     """
 
-    cdef index_t t, s, s_prime
+    cdef index_t t, s, s_prime, T, S
     cdef dtype_t v_prime
+
+    # v: shape T * S dense array of float64 objective values, init to -inf
+    # p: shape T * S dense array of float64 logprob values, init to -inf
+    # parent_s: shape T * S dense array of state values, init to arbitrary state 0
+    # parent_obs: shape T * S dense array of integer obs counts, init to 0
+
+    # Phase 1. Setup problem.
+
+    T = len(times)
+    S = len(states)
+
+    cdef dtype_t[:, :] v = numpy.empty(shape=(T, S), dtype=numpy.float64)
+    v[:, :] = -numpy.inf
+
+    cdef dtype_t[:, :] logprob = numpy.empty(shape=(T, S), dtype=numpy.float64)
+    logprob[:, :] = -numpy.inf
+
+    cdef index_t[:, :] parent_s = numpy.zeros(shape=(T, S), dtype=int)
+    cdef index_t[:, :] parent_obs = numpy.zeros(shape=(T, S), dtype=int)
+
+    # initialise value and logprob at t=0 using prior
+    for s in states:
+        v[0, s] = logprob_prior[s]
+        logprob[0, s] = logprob_prior[s]
+
+    # Phase 2. Bottom-up DP.
 
     # FIXME will not correctly handle prizes on last timestep.
     # Handle by padding with extra terminator time?
@@ -40,3 +60,31 @@ def _kernel(index_t[:] times, \
                     logprob[t+1, s_prime] = logprob_prime
                     parent_s[t+1, s_prime] = s
                     parent_obs[t+1, s_prime] = edge.delta_e
+
+    # Phase 3. Recover a value-maximising path (may be nonunique)
+    state_trajectory = []
+    obs_trajectory = []
+
+    t = T - 1
+    s = numpy.argmax(v[t, :])
+
+    objective_star = v[t, s]
+    logprob_star = logprob[t, s]
+
+    while True:
+        e = parent_obs[t, s]
+        state_trajectory.append(s)
+        obs_trajectory.append(e)
+        if t == 0:
+            break
+        t_prime = t - 1
+        s_prime = parent_s[t, s]
+        t = t_prime
+        s = s_prime
+
+    state_trajectory = list(reversed(state_trajectory))
+    obs_trajectory = list(reversed(obs_trajectory))
+
+    obs_trajectory = obs_trajectory[1:] + [0]  # FIXME aiee
+
+    return (objective_star, logprob_star, state_trajectory, obs_trajectory)
--- lib/hmmmix/trellis/slowtrellis.py
+++ lib/hmmmix/trellis/slowtrellis.py
@@ -38,18 +38,43 @@ def _search_best_path(times, states, logprob_prior, outgoing_edges, prizes):
     # states must be array of state indices 0...S-1
     # prior must be shape (S, ) array of logprob giving prior of states
 
+    # bottom up dynamic programming solve for value maximising paths.
+    if True:
+        kernel = libtrellis._kernel
+    else:
+        kernel = _kernel
+
+    objective_star, logprob_star, state_trajectory, obs_trajectory = kernel(
+        times,
+        states,
+        outgoing_edges,
+        prizes,
+        logprob_prior,
+    )
+
+    return (objective_star, logprob_star, state_trajectory, obs_trajectory)
+
+
+def _kernel(times, states, outgoing_edges, prizes, logprob_prior):
+    """
+    :param times: array of integer time indices [0, ..., T-1] shape (T,)
+    :param states: array of state indices [s_1, ..., s_K] shape (S,)
+    :param outgoing_edges: dict of state index to list of edge tuples. rework.
+    :param prizes: array of float prizes. shape (T, )
+    :param logprob_prior: array of float value. shape (S, )
+
+    returns (objective_star, logprob_star, state_trajectory, obs_trajectory)
+    """
+
+    # v: shape T * S dense array of float64 objective values, init to -inf
+    # p: shape T * S dense array of float64 logprob values, init to -inf
+    # parent_s: shape T * S dense array of state values, init to arbitrary state 0
+    # parent_obs: shape T * S dense array of integer obs counts, init to 0
+
+    # Phase 1. Setup problem.
 
     T = len(times)
     S = len(states)
-
-    """
-    storage required
-
-    v: shape T * S dense array of float64 objective values, init to -inf
-    p: shape T * S dense array of float64 logprob values, init to -inf
-    parent_s: shape T * S dense array of state values, init to arbitrary state 0
-    parent_obs: shape T * S dense array of integer obs counts, init to 0
-    """
 
     v = numpy.empty(shape=(T, S), dtype=numpy.float64)
     v[:, :] = -numpy.inf
@@ -65,19 +90,27 @@ def _search_best_path(times, states, logprob_prior, outgoing_edges, prizes):
         v[0, s] = logprob_prior[s]
         logprob[0, s] = logprob_prior[s]
 
-    # bottom up dynamic programming solve for value maximising paths.
-    if True:
-        kernel = libtrellis._kernel
-    else:
-        kernel = _kernel
+    # Phase 2. Bottom-up DP.
 
-    kernel(times, states, outgoing_edges, prizes, v, logprob, parent_s, parent_obs)
+    # FIXME will not correctly handle prizes on last timestep.
+    # Handle by padding with extra terminator time?
+    for t in times[:-1]:
+        for s in states:
+            for edge in outgoing_edges[s]:
+                s_prime = edge.succ
+                v_prime = v[t, s] + edge.weight + edge.delta_e * prizes[t]
+                logprob_prime = logprob[t, s] + edge.weight
+                if v_prime > v[t+1, s_prime]:
+                    v[t+1, s_prime] = v_prime
+                    logprob[t+1, s_prime] = logprob_prime
+                    parent_s[t+1, s_prime] = s
+                    parent_obs[t+1, s_prime] = edge.delta_e
 
-    # recover a value-maximising path (may be nonunique)
+    # Phase 3. Recover a value-maximising path (may be nonunique)
     state_trajectory = []
     obs_trajectory = []
 
-    t = T-1
+    t = T - 1
     s = numpy.argmax(v[t, :])
 
     objective_star = v[t, s]
@@ -97,36 +130,6 @@ def _search_best_path(times, states, logprob_prior, outgoing_edges, prizes):
     state_trajectory = list(reversed(state_trajectory))
     obs_trajectory = list(reversed(obs_trajectory))
 
-    obs_trajectory = obs_trajectory[1:] + [0] # FIXME aiee
+    obs_trajectory = obs_trajectory[1:] + [0]  # FIXME aiee
 
     return (objective_star, logprob_star, state_trajectory, obs_trajectory)
-
-
-def _kernel(times, states, outgoing_edges, prizes, v, logprob, parent_s, parent_obs):
-    """
-    :param times: array of integer time indices [0, ..., T-1] shape (T,)
-    :param states: array of state indices [s_1, ..., s_K] shape (S,)
-    :param outgoing_edges: dict of state index to list of edge tuples. rework.
-    :param prizes: array of float prizes. shape (T, )
-    :param v: array of float value. shape (T, S)
-    :param logprob: array of float logprob. shape (T, S)
-    :param parent_s: array of state indices. shape (T, S)
-    :param parent_obs: array of integer event counts. shape (T, S)
-
-    returns None
-
-    v, logprob, parent_s and parent_obs are mutated in place.
-    """
-    # FIXME will not correctly handle prizes on last timestep.
-    # Handle by padding with extra terminator time?
-    for t in times[:-1]:
-        for s in states:
-            for edge in outgoing_edges[s]:
-                s_prime = edge.succ
-                v_prime = v[t, s] + edge.weight + edge.delta_e * prizes[t]
-                logprob_prime = logprob[t, s] + edge.weight
-                if v_prime > v[t+1, s_prime]:
-                    v[t+1, s_prime] = v_prime
-                    logprob[t+1, s_prime] = logprob_prime
-                    parent_s[t+1, s_prime] = s
-                    parent_obs[t+1, s_prime] = edge.delta_e
