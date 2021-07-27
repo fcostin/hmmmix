c2b84eb5b6ec34e05710e3be76e857f769620d8f
rfc <reuben.fletchercostin@gmail.com>
2021-07-27 13:19:36 +1000

    refactor monthly markov event

--- lib/hmmmix/monthly_markov_event.py
+++ lib/hmmmix/monthly_markov_event.py
@@ -4,39 +4,47 @@ from .model import monthly_markov as gentrellis
 from . import trellis
 from . import markov_event
 
-_STATE_TO_INDEX = {s:i for i, s in enumerate(gentrellis.STATES)}
-_STATES = numpy.asarray([_STATE_TO_INDEX[s] for s in gentrellis.STATES], dtype=int)
-
-def _encode_edge(edge):
-    return gentrellis.Edge(succ=_STATE_TO_INDEX[edge.succ], weight=edge.weight, delta_e=edge.delta_e)
-
-_OUTGOING_EDGES = {_STATE_TO_INDEX[s]:[_encode_edge(edge) for edge in gentrellis.OUTGOING_EDGES_BY_STATE[s]] for s in gentrellis.STATES}
-
-_LOGPROB_PRIOR = numpy.asarray([gentrellis.LOGPROB_PRIOR_BY_STATE[s] for s in gentrellis.STATES], dtype=numpy.float64)
-
-_PACKED_EDGES = trellis.pack_edges(_STATES, _OUTGOING_EDGES)
-
 
 def make_hmm():
+    state_to_index = {s: i for i, s in enumerate(gentrellis.STATES)}
+    states = numpy.asarray([state_to_index[s] for s in gentrellis.STATES], dtype=int)
+
+    def _encode_edge(edge):
+        return trellis.Edge(
+            succ=state_to_index[edge.succ],
+            weight=edge.weight,
+            delta_e=edge.delta_e,
+        )
+
+    outgoing_edges = {state_to_index[s]: [_encode_edge(edge) for edge in
+                                          gentrellis.OUTGOING_EDGES_BY_STATE[s]] for s
+                      in gentrellis.STATES}
+
+    logprob_prior = numpy.asarray(
+        [gentrellis.LOGPROB_PRIOR_BY_STATE[s] for s in gentrellis.STATES],
+        dtype=numpy.float64)
+
+    packed_edges = trellis.pack_edges(states, outgoing_edges)
+
     return trellis.HMM(
-        state_by_statekey=_STATE_TO_INDEX,
-        states=_STATES,
-        packed_edges=_PACKED_EDGES,
-        prior=_LOGPROB_PRIOR,
+        state_by_statekey=state_to_index,
+        states=states,
+        packed_edges=packed_edges,
+        prior=logprob_prior,
     )
 
 
 class MonthlyMarkovEventAuxiliarySolver(markov_event.MarkovEventAuxiliarySolver):
     def __init__(self):
         super().__init__()
+        self.hmm = make_hmm()
 
     def _searchfunc(self, times, prizes_u):
         # times must be array of time indices 0...T-1
         # prizes must be shape (T, ) array of prizes (unit: logprob units per event).
 
-        hmm = make_hmm()
         objective_star, logprob_star, state_trajectory, obs_trajectory = trellis.search_best_path(
-            hmm,
+            self.hmm,
             times,
             prizes_u,
         )
